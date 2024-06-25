bf57a80dcb2b44aa1cb59835a17085a7e4c1473e
mamefall <matouf94@gmail.com>
2024-06-25 13:56:22 +0200

    Initial commit: Setup Spring Boot project

---
+++ src/main/java/com/example/unvillageunpuit/un_village_un_puit/UnVillageUnPuitApplication.java
@@ -0,0 +1,13 @@
+package com.example.unvillageunpuit.un_village_un_puit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnVillageUnPuitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnVillageUnPuitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unvillageunpuit/un_village_un_puit/UnVillageUnPuitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unvillageunpuit.un_village_un_puit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnVillageUnPuitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
